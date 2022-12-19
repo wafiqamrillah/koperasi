ef5f9815f7fdeab78ac76d3ad9805c96c1ad48a4
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-19 15:41:14 +0700

    Set menu label is index in migrations

--- database/migrations/2022_12_19_081520_create_menus_table.php
+++ database/migrations/2022_12_19_081520_create_menus_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
             $table->integer('sort')->default(0);
-            $table->string('label', 100);
+            $table->string('label', 100)->index();
             $table->enum('link_type', ['url', 'route'])->default('url')->comment("{ url : for no Laravel route generator, route : for Laravel routes }");
             $table->string('link')->default('#');
             $table->string('icon_class', 100)->nullable();
@@ -27,7 +27,7 @@ return new class extends Migration
 
         Schema::table('menus', function (Blueprint $table) {
             $table->after('id', function($table){
-                $table->foreignId('parent_id')->constrained('menus')->onDelete('cascade')->onUpdate('cascade');
+                $table->foreignId('parent_id')->nullable()->constrained('menus')->onDelete('cascade')->onUpdate('cascade');
             });
         });
     }
