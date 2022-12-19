1e9546aa532e8f196e58d47b1f3bd8c1487fd6a3
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-19 15:48:12 +0700

    Update setting table

---
+++ app/Models/System/Setting.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\System;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "description",
+        "has_translation",
+        "value",
+    ];
+
+    protected $guarded = [];
+}
