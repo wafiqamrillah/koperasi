e0457ccc18bf1cd31937c3b02ff12ff2cc83539f
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-19 15:48:18 +0700

    Update menu models

--- app/Models/System/Menu.php
+++ app/Models/System/Menu.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sort',
+        'title',
+        'parent_id',
+        'link',
+        'link_type',
+        'icon_class',
+        'has_translation',
+        'is_active',
+    ];
+
+    protected $guarded = [
+        "title",
+        "parent_id",
+        "is_active"
+    ];
+
+    protected $with = ["childs"];
+    
+    /**
+     * Get all of the childs for the Menu
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function childs()
+    {
+        return $this->hasMany(Menu::class, 'parent_id', 'id');
+    }
 }
