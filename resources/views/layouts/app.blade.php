7a40c8b1e727afc1bc8582f63fe4fcff4e5632dd
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-28 10:06:46 +0700

    Rescope app layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,52 +1,54 @@
-<x-splade-toggle>
-    <div
-        class="sidebar-mini control-sidebar-slide-open layout-fixed layout-navbar-fixed layout-footer-fixed"
-        :class="{ 'sidebar-collapse' : !toggled }">
-        <div class="wrapper">
-            <!-- Navigation -->
-            @include('layouts.navigation')
-    
-            <!-- Sidebar -->
-            <x-layouts.sidebar />
+<div>
+    <x-splade-toggle>
+        <div
+            class="sidebar-mini control-sidebar-slide-open layout-fixed layout-navbar-fixed layout-footer-fixed"
+            :class="{ 'sidebar-collapse' : !toggled }">
+            <div class="wrapper">
+                <!-- Navigation -->
+                @include('layouts.navigation')
         
-            <!-- Page Content -->
-            <main class="content-wrapper">
-                <!-- Page Heading -->
-                @if (isset($header) || isset($breadrumbs))
-                <section class="content-header">
-                    <div class="container-fluid">
-                        {{ isset($header) ? $header : null }}
-                        @isset($breadcrumbs)
-                            <!-- Breadcrumbs -->
-                            <div class="text-sm breadcrumbs">
-                                @if (is_array($breadcrumbs))
-                                    <ul>
-                                        @foreach ($breadcrumbs as $breadcrumb)
-                                            <li>{{ $breadcrumb }}</li>
-                                        @endforeach
-                                    </ul>
-                                @else
-                                    {{ $breadcrumbs }}
-                                @endif
-                            </div>
-                        @endisset
-                    </div>
-                </section>
-                @endif
-    
-                {{ $slot }}
-            </main>
-    
-            <!-- Footer -->
-            <footer class="main-footer text-sm">
-                <div class="float-right d-none d-sm-block">
-                    <b>Version</b> 3.2.0
-                </div>
-                <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-            </footer>
+                <!-- Sidebar -->
+                <x-layouts.sidebar />
             
-            <!-- Sidebar Overlay -->
-            <div id="sidebar-overlay"></div>
+                <!-- Page Content -->
+                <main class="content-wrapper">
+                    <!-- Page Heading -->
+                    @if (isset($header) || isset($breadrumbs))
+                    <section class="content-header">
+                        <div class="container-fluid">
+                            {{ isset($header) ? $header : null }}
+                            @isset($breadcrumbs)
+                                <!-- Breadcrumbs -->
+                                <div class="text-sm breadcrumbs">
+                                    @if (is_array($breadcrumbs))
+                                        <ul>
+                                            @foreach ($breadcrumbs as $breadcrumb)
+                                                <li>{{ $breadcrumb }}</li>
+                                            @endforeach
+                                        </ul>
+                                    @else
+                                        {{ $breadcrumbs }}
+                                    @endif
+                                </div>
+                            @endisset
+                        </div>
+                    </section>
+                    @endif
+        
+                    {{ $slot }}
+                </main>
+        
+                <!-- Footer -->
+                <footer class="main-footer text-sm">
+                    <div class="float-right d-none d-sm-block">
+                        <b>Version</b> 3.2.0
+                    </div>
+                    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+                </footer>
+                
+                <!-- Sidebar Overlay -->
+                <div id="sidebar-overlay"></div>
+            </div>
         </div>
-    </div>
-</x-splade-toggle>
+    </x-splade-toggle>    
+</div>
