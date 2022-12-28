75152f77138767478e8380999b8149077f6a0acc
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-28 10:59:39 +0700

    Fix app layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,54 +1,62 @@
 <div>
     <x-splade-toggle>
         <div
-            class="sidebar-mini control-sidebar-slide-open layout-fixed layout-navbar-fixed layout-footer-fixed"
-            :class="{ 'sidebar-collapse' : !toggled }">
-            <div class="wrapper">
-                <!-- Navigation -->
-                @include('layouts.navigation')
-        
-                <!-- Sidebar -->
-                <x-layouts.sidebar />
+        class="sidebar-mini control-sidebar-slide-open layout-fixed layout-navbar-fixed layout-footer-fixed"
+        :class="{ 'sidebar-collapse' : !toggled }">
+        <div class="wrapper">
+            <!-- Navigation -->
+            @include('layouts.navigation')
             
-                <!-- Page Content -->
-                <main class="content-wrapper">
-                    <!-- Page Heading -->
-                    @if (isset($header) || isset($breadrumbs))
-                    <section class="content-header">
-                        <div class="container-fluid">
-                            {{ isset($header) ? $header : null }}
-                            @isset($breadcrumbs)
+            <!-- Sidebar -->
+            <x-layouts.sidebar />
+            
+            <!-- Page Content -->
+            <main class="content-wrapper">
+                <!-- Page Heading -->
+                @if (isset($header) || isset($breadrumbs))
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                {{ isset($header) ? $header : null }}
+                            </div>
+                            <div class="col-sm-6">
                                 <!-- Breadcrumbs -->
-                                <div class="text-sm breadcrumbs">
+                                <ol class="breadcrumb float-sm-right">
+                                @isset($breadcrumbs)
                                     @if (is_array($breadcrumbs))
-                                        <ul>
-                                            @foreach ($breadcrumbs as $breadcrumb)
-                                                <li>{{ $breadcrumb }}</li>
-                                            @endforeach
-                                        </ul>
+                                        @foreach ($breadcrumbs as $breadcrumb)
+                                        <li>{{ $breadcrumb }}</li>
+                                        @endforeach
                                     @else
                                         {{ $breadcrumbs }}
                                     @endif
-                                </div>
-                            @endisset
+                                @endisset
+                                </ol>
+                            </div>
                         </div>
-                    </section>
-                    @endif
-        
-                    {{ $slot }}
-                </main>
-        
-                <!-- Footer -->
-                <footer class="main-footer text-sm">
-                    <div class="float-right d-none d-sm-block">
-                        <b>Version</b> 3.2.0
                     </div>
-                    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-                </footer>
+                </section>
+                @endif
                 
-                <!-- Sidebar Overlay -->
-                <div id="sidebar-overlay"></div>
-            </div>
+                <section class="content">
+                    <div class="container-fluid">
+                        {{ $slot }}
+                    </div>
+                </section>
+            </main>
+            
+            <!-- Footer -->
+            <footer class="main-footer text-sm">
+                <div class="float-right d-none d-sm-block">
+                    <b>Version</b> 3.2.0
+                </div>
+                <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+            </footer>
+            
+            <!-- Sidebar Overlay -->
+            <div id="sidebar-overlay"></div>
         </div>
-    </x-splade-toggle>    
+    </div>
+</x-splade-toggle>    
 </div>
