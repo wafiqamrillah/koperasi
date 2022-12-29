e521dd494eac2b48aa519717b2555ba2602599ed
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-29 09:29:41 +0700

    Fix z-index of toastr

--- resources/views/vendor/splade/components/toast-wrapper.blade.php
+++ resources/views/vendor/splade/components/toast-wrapper.blade.php
@@ -5,37 +5,23 @@
             animation="opacity"
             appear
             show="toasts.hasBackdrop"
-            class="fixed z-30 inset-0 bg-black/75"
-            style="position: fixed; z-index: 30; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(0, 0, 0, .75);"
+            class="tw-fixed tw-z-[1200] tw-inset-0 tw-bg-black/75"
         />
 
-        <div
-            style="
-                position: fixed;
-                z-index: 40;
-                top: 0px;
-                right: 0px;
-                bottom: 0px;
-                left: 0px;
-                display: grid;
-                grid-template-columns: repeat(1, minmax(0, 1fr));
-                grid-auto-flow: row;
-                pointer-events: none;
-            ">
-            <div v-for="position in toasts.positions" style="position: relative">
-                <div :style="{
-                        'position: absolute; width: 100%; height: 100%; display: flex; flex-direction: column; padding: 1rem; margin-top: 1rem;' : true,
-                        'align-items: flex-start; justify-content: flex-start;': position == 'left-top',
-                        'align-items: center; justify-content: flex-start;': position == 'center-top',
-                        'align-items: flex-end; justify-content: flex-start;': position == 'right-top',
-                        'align-items: flex-start; justify-content: center;': position == 'left-center',
-                        'align-items: center; justify-content: center;': position == 'center-center',
-                        'align-items: flex-end; justify-content: center;': position == 'right-center',
-                        'align-items: flex-start; justify-content: flex-end;': position == 'left-bottom',
-                        'align-items: center; justify-content: flex-end;': position == 'center-bottom',
-                        'align-items: flex-end; justify-content: flex-end;': position == 'right-bottom'
-                    }"
-                >
+        <div class="tw-fixed tw-z-[1201] tw-inset-0 tw-grid tw-grid-cols-3 tw-grid-flow-row-3 tw-pointer-events-none">
+            <div v-for="position in toasts.positions" class="tw-relative">
+                <div :class="{
+                    'tw-absolute tw-w-full tw-h-full tw-flex tw-flex-col tw-p-4 tw-space-y-4': true,
+                    'tw-items-start tw-justify-start': position == 'left-top',
+                    'tw-items-center tw-justify-start': position == 'center-top',
+                    'tw-items-end tw-justify-start': position == 'right-top',
+                    'tw-items-start tw-justify-center': position == 'left-center',
+                    'tw-items-center tw-justify-center': position == 'center-center',
+                    'tw-items-end tw-justify-center': position == 'right-center',
+                    'tw-items-start tw-justify-end': position == 'left-bottom',
+                    'tw-items-center tw-justify-end': position == 'center-bottom',
+                    'tw-items-end tw-justify-end': position == 'right-bottom'
+                }">
                     <template v-for="(toast, toastKey) in toasts.toasts">
                         <template v-if="toast.position == position && !toast.dismissed && toast.html">
                             <SpladeRender
