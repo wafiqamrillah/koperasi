94718a7d1b184a1f074e3f1afa559037130ea23d
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-30 09:46:28 +0700

    Make Gate in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Grant super user and developer to have all permissions
+        Gate::before(function($user, $ability) {
+            return $user->hasRole('super user') || $user->hasRole('developer') ? true : null;
+        });
     }
 }
