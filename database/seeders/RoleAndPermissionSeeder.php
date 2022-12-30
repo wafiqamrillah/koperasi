1f78f19b331b7e247031b9a9e012f75334f51790
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-30 09:11:16 +0700

    Make role and permission seeder

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\{ Role, Permission };
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = collect([
+            'super user',
+            'developer'
+        ]);
+
+        $permissions = collect([
+            'access settings',
+            'update settings'
+        ]);
+
+        try {
+            \DB::transaction(function () use($roles, $permissions) {
+                $roles = $roles->map(function($role) {
+                    return Role::firstOrCreate(['name' => $role]);
+                });
+
+                $permissions = $permissions->map(function($permission) {
+                    return Permission::firstOrCreate(['name' => $permission]);
+                });
+
+                $roles->each(function($role) use($permissions) {
+                    $role->syncPermissions($permissions);
+                });
+            });
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+}
