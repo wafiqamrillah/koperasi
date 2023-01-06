8efbaa7dc8a8ef35de0c2203e740521a68f54877
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-01-06 10:37:39 +0700

    Font small on body

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,62 +1,62 @@
 <div class="tw-relative">
     <x-splade-toggle>
         <div
-        class="sidebar-mini control-sidebar-slide-open layout-fixed layout-navbar-fixed layout-footer-fixed"
-        :class="{ 'sidebar-collapse sidebar-close' : !toggled, 'sidebar-open' : toggled }">
-        <div class="wrapper">
-            <!-- Navigation -->
-            @include('layouts.navigation')
-            
-            <!-- Sidebar -->
-            <x-layouts.sidebar />
-            
-            <!-- Page Content -->
-            <main class="content-wrapper">
-                <!-- Page Heading -->
-                @if (isset($header) || isset($breadrumbs))
-                <section class="content-header">
-                    <div class="container-fluid">
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                {{ isset($header) ? $header : null }}
-                            </div>
-                            <div class="col-sm-6">
-                                <!-- Breadcrumbs -->
-                                <ol class="breadcrumb float-sm-right">
-                                @isset($breadcrumbs)
-                                    @if (is_array($breadcrumbs))
-                                        @foreach ($breadcrumbs as $breadcrumb)
-                                        <li>{{ $breadcrumb }}</li>
-                                        @endforeach
-                                    @else
-                                        {{ $breadcrumbs }}
-                                    @endif
-                                @endisset
-                                </ol>
+            class="sidebar-mini control-sidebar-slide-open layout-fixed layout-navbar-fixed layout-footer-fixed"
+            :class="{ 'sidebar-collapse sidebar-close' : !toggled, 'sidebar-open' : toggled }">
+            <div class="wrapper">
+                <!-- Navigation -->
+                @include('layouts.navigation')
+                
+                <!-- Sidebar -->
+                <x-layouts.sidebar />
+                
+                <!-- Page Content -->
+                <main class="content-wrapper text-sm">
+                    <!-- Page Heading -->
+                    @if (isset($header) || isset($breadrumbs))
+                    <section class="content-header">
+                        <div class="container-fluid">
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    {{ isset($header) ? $header : null }}
+                                </div>
+                                <div class="col-sm-6">
+                                    <!-- Breadcrumbs -->
+                                    <ol class="breadcrumb float-sm-right">
+                                    @isset($breadcrumbs)
+                                        @if (is_array($breadcrumbs))
+                                            @foreach ($breadcrumbs as $breadcrumb)
+                                            <li>{{ $breadcrumb }}</li>
+                                            @endforeach
+                                        @else
+                                            {{ $breadcrumbs }}
+                                        @endif
+                                    @endisset
+                                    </ol>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-                @endif
+                    </section>
+                    @endif
+                    
+                    <section class="content">
+                        <div class="container-fluid">
+                            {{ $slot }}
+                        </div>
+                    </section>
+                </main>
                 
-                <section class="content">
-                    <div class="container-fluid">
-                        {{ $slot }}
+                <!-- Footer -->
+                <footer class="main-footer text-sm">
+                    <div class="float-right d-none d-sm-block">
+                        <b>Version</b> 3.2.0
                     </div>
-                </section>
-            </main>
-            
-            <!-- Footer -->
-            <footer class="main-footer text-sm">
-                <div class="float-right d-none d-sm-block">
-                    <b>Version</b> 3.2.0
-                </div>
-                <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-            </footer>
-            
-            <!-- Sidebar Overlay -->
-            <div id="sidebar-overlay" @click.prevent="setToggle(false)"></div>
+                    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+                </footer>
+                
+                <!-- Sidebar Overlay -->
+                <div id="sidebar-overlay" @click.prevent="setToggle(false)"></div>
+            </div>
         </div>
-    </div>
-</x-splade-toggle>    
+    </x-splade-toggle>    
 </div>
