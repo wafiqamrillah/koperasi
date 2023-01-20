cfb8381ab4b4c88f742ee9940c271ce02cf8deaa
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-01-20 09:34:13 +0700

    Fix sidebar menu

--- resources/views/components/layouts/sidebar/menu.blade.php
+++ resources/views/components/layouts/sidebar/menu.blade.php
@@ -1,5 +1,9 @@
 <li class="nav-item">
+    @if ($link !== "#")
     <Link href="{{ $link }}" class="nav-link">
+    @else
+    <a href="#" class="nav-link">
+    @endif
         <i class="nav-icon {{ $icon_class }}"></i>
         <p>
             {{ $has_translation ? __($menu['label']) : $menu['label'] }}
@@ -7,7 +11,11 @@
                 <i class="right fa-solid fa-angle-left"></i>
             @endif
         </p>
+    @if ($link !== "#")
     </Link>
+    @else
+    </a>
+    @endif
 
     @if ($childs->count() > 0)
         <ul class="nav nav-treeview">
