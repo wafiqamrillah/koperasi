77985ae61aa449fdf08a33d117a9b0c513ab0010
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-01-23 10:24:59 +0700

    Making SidebarMenu in Vue languages

---
+++ resources/js/Components/Layouts/Sidebar/SidebarMenu.vue
@@ -0,0 +1,45 @@
+<script setup>
+    const props = defineProps({
+        menu: {
+            type: Object,
+            default : {}
+        }
+    });
+
+    const sameLink = window.location.href === props.menu?.link;
+</script>
+
+<template>
+    <li class="nav-item">
+        <template v-if="menu.link !== '#'">
+            <Link :href="menu.link" class="nav-link" :class="{ 'active' : sameLink  }">
+                <i :class="`nav-icon ${menu?.icon_class}`"></i>
+                <p>
+                    {{ menu.label }}
+                    <template v-if="(menu?.childs ? menu.childs : []).length > 0">
+                        <i class="right fa-solid fa-angle-left"></i>
+                    </template>
+                </p>
+            </Link>
+        </template>
+        <template v-else>
+            <a :href="menu.link" class="nav-link">
+                <i :class="`nav-icon ${menu?.icon_class}`"></i>
+                <p>
+                    {{ menu.label }}
+                    <template v-if="(menu?.childs ? menu.childs : []).length > 0">
+                        <i class="right fa-solid fa-angle-left"></i>
+                    </template>
+                </p>
+            </a>
+        </template>
+        
+        <template v-if="(menu?.childs ? menu.childs : []).length > 0">
+            <ul class="nav nav-treeview">
+                <template v-for="(child, index) in menu.childs" :key="index">
+                    <SidebarMenu :menu="child" />
+                </template>
+            </ul>
+        </template>
+    </li>
+</template>
