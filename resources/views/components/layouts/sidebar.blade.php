d346c07b5e46b588b77ecee837ecd406155ba73a
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-02-14 09:15:57 +0700

    Text sm on sidebar

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -22,7 +22,7 @@
         
         <!-- Sidebar menus -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent text-sm" data-widget="treeview" role="menu" data-accordion="false">
                 @foreach($menus as $menu)
                     {{-- <x-layouts.sidebar.menu :menu="collect($menu)" /> --}}
                     <SidebarMenu :menu="@js($menu)" />
